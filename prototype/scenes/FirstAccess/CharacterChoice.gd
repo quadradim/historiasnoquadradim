5ad57728b6d2ca37e4094323f655c937192c2bb6
lucaSena <87825212+lucaSena@users.noreply.github.com>
2022-03-03 01:13:28 -0300

    Escolha de personagem #5

--- prototype/scenes/FirstAccess/CharacterChoice.gd
+++ prototype/scenes/FirstAccess/CharacterChoice.gd
@@ -1,7 +1,11 @@
 extends CanvasLayer
 
-func _ready():
-	pass # Replace with function body.
+export(Resource) var image_source
 
+func _ready():
+	$TabContainer/Habilidades/Abiliity.texture = image_source
+	pass # Replace with function body.
+	
 func choose_an_object(object_source):
+	$TabContainer/Habilidades/Abiliity.texture = object_source
 	print(object_source)
