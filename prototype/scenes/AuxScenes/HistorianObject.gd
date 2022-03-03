dc99867f6b4bbe09a777708a2abd8f4f896dcced
lucaSena <87825212+lucaSena@users.noreply.github.com>
2022-03-03 13:01:40 -0300

    Escolha de personagem #8

--- prototype/scenes/AuxScenes/HistorianObject.gd
+++ prototype/scenes/AuxScenes/HistorianObject.gd
@@ -4,14 +4,19 @@ export(Resource) var object_source
 export(String) var description
 signal clicked_object(object_source,description)
 
-
 func _ready():
 	$Image.apply_scale(Vector2(0.8, 0.8))
 	$Image.texture = object_source
-
+	
 func get_image():
 	return object_source
 	
 func click_object(event):
 	if event.is_pressed() and event.button_index == BUTTON_LEFT:
 		emit_signal("clicked_object", object_source,description)
+		
+func select_object():
+	$Background.color = '#d5c2ff'
+func unselect_object():
+	$Background.color = '#fef8dd'
+		
--- prototype/scenes/FirstAccess/CharacterChoice.gd
+++ prototype/scenes/FirstAccess/CharacterChoice.gd
@@ -1,14 +1,42 @@
 extends CanvasLayer
 
+var num_selected = []
+var items = []
+
 export(Resource) var image_source
 
 func _ready():
 	$TabContainer/Habilidades/Abiliity.texture = image_source
 	$TabContainer/Habilidades/AbiliityDescription.text = "Pikachu"
-	pass # Replace with function body.
-	
+	items.append($TabContainer/Habilidades/img1)
+	items.append($TabContainer/Habilidades/img2)
+	items.append($TabContainer/Habilidades/img3)
+	items.append($TabContainer/Habilidades/img4)
+	items.append($TabContainer/Habilidades/img5)
+	items.append($TabContainer/Habilidades/img6)
+	items.append($TabContainer/Habilidades/img7)
+	items.append($TabContainer/Habilidades/img8)
+	items.append($TabContainer/Habilidades/img9)
+
 func choose_an_object(object_source,description):
 	$TabContainer/Habilidades/Abiliity.texture = object_source
 	$TabContainer/Habilidades/AbiliityDescription.text = description
-	
+	for item in items:
+		if num_selected.size() < 3:
+			if object_source == item.get_image():
+				if object_source in num_selected:
+					num_selected.erase(object_source)
+					item.unselect_object()
+				else:
+					num_selected.append(object_source)
+					item.select_object()
+				break
+		elif object_source in num_selected and object_source == item.get_image():
+			num_selected.erase(object_source)
+			item.unselect_object()
+			break
 	print(object_source)
+
+
+
+
