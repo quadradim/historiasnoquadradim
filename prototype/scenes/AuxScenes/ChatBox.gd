09d0126d9201ea96024e89c6237cceb68d6887f9
uventura <ualivent@gmail.com>
2022-03-29 20:48:26 -0300

    Changing Dialog Scene

--- prototype/scenes/AuxScenes/ChatBox.gd
+++ prototype/scenes/AuxScenes/ChatBox.gd
@@ -1,23 +1,40 @@
 extends Control
 
 
-export(String) var chat_text 
+#export(String) var chat_text 
+export(String, FILE) var chat_res
 
 var text_position = 0
 var current_text_pos = 0
 var current_text = ''
 var presses_count = 0
 
+var current_dialog = []
 
 func write_text():
 	pass
+	
+func load_file(file_src):
+	var file = File.new()
+	file.open(file_src, File.READ)
+	
+	var dialog = ""
+	
+	while not file.eof_reached():
+		var current_line = file.get_line()
+		if current_line == "**":
+			current_dialog.append(dialog)
+			dialog = ""
+		else:
+			dialog += current_line
+	file.close()
 
 func _ready():
-	pass # Replace with function body.
+	load_file(str(chat_res))
 		
 func Time_to_write():
-	if current_text_pos < len(chat_text[text_position]) :
-		current_text += chat_text[text_position][current_text_pos]
+	if current_text_pos < len(current_dialog[text_position]) :
+		current_text += current_dialog[text_position][current_text_pos]
 		current_text_pos += 1
 		$ChatText.text = current_text
 	$Timer.start()
@@ -27,7 +44,7 @@ func Time_to_write():
 
 
 func _on_Button2_pressed():
-	if presses_count < chat_text.size() - 1:
+	if presses_count < current_dialog.size() - 1:
 		$ChatText.text = ''
 		text_position+=1
 		current_text = ''
