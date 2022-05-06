3b1ac82986eb09910f36d5c4761acf0403827ed1
uventura <ualivent@gmail.com>
2022-05-05 21:49:24 -0300

    Criando paleta de cores padrão

--- prototype/scenes/FirstAccess/CharacterChoice.gd
+++ prototype/scenes/FirstAccess/CharacterChoice.gd
@@ -8,26 +8,26 @@ export(Resource) var image_source
 signal end_choice
 
 func _ready():
-	$TabContainer/Habilidades/Abiliity.texture = image_source
-	$TabContainer/Habilidades/AbiliityDescription.text = "Pikachu"
-	items.append($TabContainer/Habilidades/img1)
-	items.append($TabContainer/Habilidades/img2)
-	items.append($TabContainer/Habilidades/img3)
-	items.append($TabContainer/Habilidades/img4)
-	items.append($TabContainer/Habilidades/img5)
-	items.append($TabContainer/Habilidades/img6)
-	items.append($TabContainer/Habilidades/img7)
-	items.append($TabContainer/Habilidades/img8)
-	items.append($TabContainer/Habilidades/img9)
+	$TabContainer/Habilities/Abiliity.texture = image_source
+	$TabContainer/Habilities/AbiliityDescription.text = "Pikachu"
+	items.append($TabContainer/Habilities/HabilityPanel/img1)
+	items.append($TabContainer/Habilities/HabilityPanel/img2)
+	items.append($TabContainer/Habilities/HabilityPanel/img3)
+	items.append($TabContainer/Habilities/HabilityPanel/img4)
+	items.append($TabContainer/Habilities/HabilityPanel/img5)
+	items.append($TabContainer/Habilities/HabilityPanel/img6)
+	items.append($TabContainer/Habilities/HabilityPanel/img7)
+	items.append($TabContainer/Habilities/HabilityPanel/img8)
+	items.append($TabContainer/Habilities/HabilityPanel/img9)
 
 	var sizeto=Vector2(400,400)
 	var size=image_source.get_size()
 	var scale_factor=sizeto/size
-	$TabContainer/Habilidades/Abiliity.scale=scale_factor
+	$TabContainer/Habilities/Abiliity.scale=scale_factor
 
 func choose_an_object(object_source,description):
-	$TabContainer/Habilidades/Abiliity.texture = object_source
-	$TabContainer/Habilidades/AbiliityDescription.text = description
+	$TabContainer/Habilities/Abiliity.texture = object_source
+	$TabContainer/Habilities/AbiliityDescription.text = description
 	for item in items:
 		if num_selected.size() < 3:
 			if object_source == item.get_image():
