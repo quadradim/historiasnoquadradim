469622b33fd9ef41802b2cc205bede9f63921233
lucaSena <87825212+lucaSena@users.noreply.github.com>
2022-05-13 16:33:28 -0300

    Rearranjando pastas e arrumando estilos

--- prototype/scenes/FirstAccess/CharacterChoice.gd
+++ prototype/scenes/FirstAccess/CharacterChoice.gd
@@ -8,26 +8,26 @@ export(Resource) var image_source
 signal end_choice
 
 func _ready():
-	$TabContainer/Habilities/Abiliity.texture = image_source
-	$TabContainer/Habilities/AbiliityDescription.text = "Pikachu"
-	items.append($TabContainer/Habilities/HabilityPanel/img1)
-	items.append($TabContainer/Habilities/HabilityPanel/img2)
-	items.append($TabContainer/Habilities/HabilityPanel/img3)
-	items.append($TabContainer/Habilities/HabilityPanel/img4)
-	items.append($TabContainer/Habilities/HabilityPanel/img5)
-	items.append($TabContainer/Habilities/HabilityPanel/img6)
-	items.append($TabContainer/Habilities/HabilityPanel/img7)
-	items.append($TabContainer/Habilities/HabilityPanel/img8)
-	items.append($TabContainer/Habilities/HabilityPanel/img9)
+	$TabContainer/Habilidades/Abiliity.texture = image_source
+	$TabContainer/Habilidades/AbiliityDescription.text = "Selecione 3 habilidades"
+	items.append($TabContainer/Habilidades/HabilityPanel/img1)
+	items.append($TabContainer/Habilidades/HabilityPanel/img2)
+	items.append($TabContainer/Habilidades/HabilityPanel/img3)
+	items.append($TabContainer/Habilidades/HabilityPanel/img4)
+	items.append($TabContainer/Habilidades/HabilityPanel/img5)
+	items.append($TabContainer/Habilidades/HabilityPanel/img6)
+	items.append($TabContainer/Habilidades/HabilityPanel/img7)
+	items.append($TabContainer/Habilidades/HabilityPanel/img8)
+	items.append($TabContainer/Habilidades/HabilityPanel/img9)
 
-	var sizeto=Vector2(400,400)
+	var sizeto=Vector2(200,200)
 	var size=image_source.get_size()
 	var scale_factor=sizeto/size
-	$TabContainer/Habilities/Abiliity.scale=scale_factor
+	$TabContainer/Habilidades/Abiliity.scale=scale_factor
 
 func choose_an_object(object_source,description):
-	$TabContainer/Habilities/Abiliity.texture = object_source
-	$TabContainer/Habilities/AbiliityDescription.text = description
+	$TabContainer/Habilidades/Abiliity.texture = object_source
+	$TabContainer/Habilidades/AbiliityDescription.text = description
 	for item in items:
 		if num_selected.size() < 3:
 			if object_source == item.get_image():
