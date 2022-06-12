0f2effc30252b9bc9cd7d7f29016f755b2bda7ba
uventura <ualivent@gmail.com>
2022-06-12 17:56:56 -0300

    Finish visual design from character creation

--- prototype/scenes/AuxScenes/ObjectSelection.gd
+++ prototype/scenes/AuxScenes/ObjectSelection.gd
@@ -1,12 +1,17 @@
 extends Node
 
-signal clicked_object(object_source,description)
+signal clicked_object(object_id, object_source, object_name, description)
+
+var object_id
 export(Resource) var object_source
+#export(Resource) var object_source_zoom
 export(String) var object_name
 export(String) var description
 var style = false
 
 func _ready():
+	object_id = self
+	
 	# Image Settings
 	if !object_source:
 		var source = Resource.new()
@@ -15,24 +20,24 @@ func _ready():
 		object_source = source
 		
 	$Background/Object.texture = object_source
-	$Background/Object.scale = Vector2(1.1, 1.1)
+	$Background/Object.scale = Vector2(1, 1)
 	
 	# Background Style Settings
 	style = StyleBoxFlat.new()
-	style.set_bg_color(Color('#fef8dd'))
+	style.set_bg_color(Color('#f8e16f'))
 	style.set_corner_radius_all(20)
-	style.set_border_width_all(4)
-	style.set_border_color(Color('#fef8dd'))
+	style.set_border_width_all(10)
+	style.set_border_color(Color('#f8e16f'))
 	
 	var background = get_node("Background")
 	background.set('custom_styles/panel', style)
 
 func clicked_object(event):
 	if event.is_pressed() and event.button_index == BUTTON_LEFT:
-		emit_signal("clicked_object", object_source,description)
+		emit_signal("clicked_object", object_id, object_source, object_name, description)
 
 func select_object():
 	style.set_border_color(Color('#d0a473'))
 	
 func unselect_object():
-	style.set_border_color(Color('#fef8dd'))
+	style.set_border_color(Color('#f8e16f'))
--- prototype/scenes/FirstAccess/CharacterChoice.gd
+++ prototype/scenes/FirstAccess/CharacterChoice.gd
@@ -1,44 +1,74 @@
 extends CanvasLayer
 
-var num_selected = []
 var items = []
-var style = false
+var id_item_selected = []
+var selected_backpack = false
 
 export(Resource) var image_source
 signal end_choice
 
 func _ready():
+	# Message Box text style
+	$MessageBox/MessageText.add_color_override("font_color", Color('#000000'))
+	
+	# Button starts disabled
+	$ContinueButton.set_disabled(true)
+	
 	# Panel Style Settings
-	style = StyleBoxFlat.new()
-	style.set_bg_color(Color('#f8e16f'))
-	style.set_corner_radius_all(5)
+	var historian_style = StyleBoxFlat.new()
+	historian_style.set_bg_color(Color('#f1eee7'))
+	historian_style.set_corner_radius_all(5)
 	
-	var hability_panel = get_node("TabContainer/Habilidades/HabilityPanel")
-	hability_panel.set('custom_styles/panel', style)
+	var hability_panel = get_node("TabContainer/Habilidades/CreationPanel")
+	hability_panel.set('custom_styles/panel', historian_style)
 	
-	var sizeto=Vector2(200,200)
-	var size=image_source.get_size()
-	var scale_factor=sizeto/size
-	#$TabContainer/Habilidades/Abiliity.scale=scale_factor
+	var style_panel = get_node("TabContainer/Estilo/CreationPanel")
+	style_panel.set('custom_styles/panel', historian_style)
+	
+	items.append($TabContainer/Habilidades/CreationPanel/hab01)
+	items.append($TabContainer/Habilidades/CreationPanel/hab02)
+	items.append($TabContainer/Habilidades/CreationPanel/hab03)
+	items.append($TabContainer/Habilidades/CreationPanel/hab04)
+	items.append($TabContainer/Habilidades/CreationPanel/hab05)
+	items.append($TabContainer/Habilidades/CreationPanel/hab06)
+	items.append($TabContainer/Habilidades/CreationPanel/hab07)
+	items.append($TabContainer/Habilidades/CreationPanel/hab08)
+	items.append($TabContainer/Habilidades/CreationPanel/hab09)
+	
+	# Object Selected with Zoom
+	#90b9e1, 66a1dc
+	var historian_object_zoom_style = StyleBoxFlat.new()
+	historian_object_zoom_style.set_bg_color(Color('#f1eee7'))
+	historian_object_zoom_style.set_corner_radius_all(5)
+	historian_object_zoom_style.set_border_width_all(7)
+	historian_object_zoom_style.set_border_color(Color('#66a1dc'))
+	
+	var historian_object_zoom = get_node("TabContainer/Habilidades/HabilityZoomImage")
+	historian_object_zoom.set('custom_styles/panel', historian_object_zoom_style)
+	
 
-func choose_an_object(object_source,description):
-	$TabContainer/Habilidades/Abiliity.texture = object_source
-	$TabContainer/Habilidades/AbiliityDescription.text = description
-	for item in items:
-		if num_selected.size() < 3:
-			if object_source == item.get_image():
-				if object_source in num_selected:
-					num_selected.erase(object_source)
-					item.unselect_object()
-				else:
-					num_selected.append(object_source)
-					item.select_object()
-				break
-		elif object_source in num_selected and object_source == item.get_image():
-			num_selected.erase(object_source)
-			item.unselect_object()
-			break
-	print(object_source)
+func choose_an_hability(object_id, object_source,object_name,description):
+	$TabContainer/Habilidades/HabilityZoomImage/HabiliityImage.texture = object_source
+	$TabContainer/Habilidades/HabilityZoomImage/HabilityZoomImageName/HabilityName.text = object_name
+	$TabContainer/Habilidades/HabilityZoomImage/HabilityDescriptionPanel/HabilityDescription.text = description
+	
+	if object_id in id_item_selected:
+		object_id.unselect_object()
+		id_item_selected.erase(object_id)
+	elif id_item_selected.size() < 3:
+		object_id.select_object()
+		id_item_selected.append(object_id)
+
+
+func choose_a_backpack(object_id, object_source,object_name,description):
+	if not selected_backpack:
+		selected_backpack = object_id
+	selected_backpack.unselect_object()
+	
+	selected_backpack = object_id
+	selected_backpack.select_object()
+	
+	$TabContainer/Estilo/BackpackZoom.texture = object_source
 
 func end_character_choice():
 	emit_signal("end_choice")
