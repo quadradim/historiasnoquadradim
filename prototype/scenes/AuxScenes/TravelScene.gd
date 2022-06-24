c59aceda98f9ff00fd8ce9f1aeddc8549203d98d
lucaSena <87825212+lucaSena@users.noreply.github.com>
2022-06-24 16:17:17 -0300

    TravelScene improvement

--- prototype/scenes/AuxScenes/TravelScene.gd
+++ prototype/scenes/AuxScenes/TravelScene.gd
@@ -3,6 +3,11 @@ extends CanvasLayer
 onready var transition_animation = $TransitionAnimation
 onready var transition_animation_name = "fade_out"
 
+var text_position = 0
+var current_text_pos = 0
+var current_text = ''
+var text_tutorials =['Escolha seu transporte de acordo com o destino desejado.']
+
 const Nucleo = preload("res://scenes/Levels/NúcleoBandeirante.tscn")
 const Tagua = preload("res://scenes/Levels/Taguatinga.tscn")
 const Esplanada = preload("res://scenes/Levels/Esplanada.tscn")
@@ -14,6 +19,7 @@ var travel_destiny = 'a'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_on_Timer_timeout()
 	pass # Replace with function body.
 
 func _on_GoToNcleo_button_down():
@@ -46,3 +52,11 @@ func _on_Node_transitioned_to_esplanada():
 
 func _on_Node_transitioned_to_tagua():
 	$CourrentScene.add_child(Tagua.instance())
+
+
+func _on_Timer_timeout():
+	if current_text_pos < len(text_tutorials[text_position]):
+		current_text += text_tutorials[text_position][current_text_pos]
+		current_text_pos += 1
+		$HelpText.text = current_text
+	pass # Replace with function body.
