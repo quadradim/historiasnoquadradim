b14e42e3bed7a26b1abc35a294c78b4d2201827f
uventura <ualivent@gmail.com>
2022-07-10 19:17:47 -0300

    Creating a diary scene
    Until now we had a temporary diary image, now is being used a sketch.

--- prototype/scenes/GUI/Diary.gd
+++ prototype/scenes/GUI/Diary.gd
@@ -1,11 +1,25 @@
 extends CanvasLayer
 
+var player_data
+
 func _ready():
-	# Panel Style Settings
-#	var pages_style = StyleBoxFlat.new()
-#	pages_style.set_bg_color(Color(1,1,1,0))
-#	pages_style.set_corner_radius_all(5)
-#
-#	var pages = get_node("ImagesTab")
-#	pages.set('custom_styles/panel', pages_style)
-	pass
+	player_data = $PlayerEntitiy.read()
+	
+	# Load backpack
+	$ImagesTab/ProfileTab/BackpackPanel/Backpack.texture = load(player_data["backpack"])
+	
+	# Load Habilities
+	$ImagesTab/ProfileTab/ItemPanel01/hability.texture = load(player_data["habilities"][0][0])
+	$ImagesTab/ProfileTab/ItemPanel02/hability.texture = load(player_data["habilities"][1][0])
+	$ImagesTab/ProfileTab/ItemPanel03/hability.texture = load(player_data["habilities"][2][0])
+	
+	# Load Name
+	$ImagesTab/ProfileTab/HistorianName.text = player_data["name"]
+	
+	# New Message Popup
+	$FirstAccessMessage.popup()
+	$FirstAccessMessageAnimation.play("NewGame")
+
+
+func _on_Button_pressed():
+	emit_signal("end_diary")
