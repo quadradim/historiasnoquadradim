2e28194d19c0d4b17dd7ee34084ca5ed9a23df2b
lucaSena <87825212+lucaSena@users.noreply.github.com>
2022-08-20 21:26:10 -0300

    dialog solution

--- prototype/scenes/AuxScenes/ProfileImages.gd
+++ prototype/scenes/AuxScenes/ProfileImages.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-var texture = ImageTexture.new()
-var image = Image.new()
-export var images = ["res://assets/Images/Cenas/Lupa.png"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,14 +9,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_ChatBox_player_dialog():
-	image.load("res://assets/Images/Cenas/Lupa.png")
-	texture.create_from_image(image)
-	$Background/Image.texture = image
+	
+func display(image,scal):
+	$Image.scale = scal
+	$Image.texture = image
 	pass # Replace with function body
 
 
 func _on_ChatBox_suzana_dialog():
 	pass # Replace with function body.
+
