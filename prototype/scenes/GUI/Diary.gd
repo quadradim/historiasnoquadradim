ee3e204fe072609fcc78426993e1a402bac6b816
lucaSena <87825212+lucaSena@users.noreply.github.com>
2022-08-29 02:09:05 -0300

    Diary Update

--- prototype/scenes/GUI/Diary.gd
+++ prototype/scenes/GUI/Diary.gd
@@ -2,20 +2,40 @@ extends CanvasLayer
 
 var player_data
 signal end_diary
+var locked_characters = [1,0,0,0,0,0,0,0,0]
 
 func _ready():
+	if bool(locked_characters[0]) == false:
+		$CharactersTab/Character1.texture = null
+	if bool(locked_characters[1]) == false:
+		$CharactersTab/Character2.texture = null
+	if bool(locked_characters[2]) == false:
+		$CharactersTab/Character3.texture = null
+	if bool(locked_characters[3]) == false:
+		$CharactersTab/Character4.texture = null
+	if bool(locked_characters[4]) == false:
+		$CharactersTab/Character5.texture = null
+	if bool(locked_characters[5]) == false:
+		$CharactersTab/Character6.texture = null
+	if bool(locked_characters[6]) == false:
+		$CharactersTab/Character7.texture = null
+	if bool(locked_characters[7]) == false:
+		$CharactersTab/Character8.texture = null
+	if bool(locked_characters[8]) == false:
+		$CharactersTab/Character9.texture = null
+	
 	player_data = $PlayerEntitiy.read()
 	
 	# Load backpack
-	$ImagesTab/ProfileTab/BackpackPanel/Backpack.texture = load(player_data["backpack"])
+	$ProfileTab/BackpackPanel/Backpack.texture = load(player_data["backpack"])
 	
 	# Load Habilities
-	$ImagesTab/ProfileTab/ItemPanel01/hability.texture = load(player_data["habilities"][0][0])
-	$ImagesTab/ProfileTab/ItemPanel02/hability.texture = load(player_data["habilities"][1][0])
-	$ImagesTab/ProfileTab/ItemPanel03/hability.texture = load(player_data["habilities"][2][0])
+	$ProfileTab/ItemPanel01/hability.texture = load(player_data["habilities"][0][0])
+	$ProfileTab/ItemPanel02/hability.texture = load(player_data["habilities"][1][0])
+	$ProfileTab/ItemPanel03/hability.texture = load(player_data["habilities"][2][0])
 	
 	# Load Name
-	$ImagesTab/ProfileTab/HistorianName.text = player_data["name"]
+	$ProfileTab/HistorianName.text = player_data["name"]
 	
 	# New Message Popup
 	$FirstAccessMessage.popup()
@@ -32,3 +52,46 @@ func play_music():
 
 func music_finished():
 	$MainMusic.play()
+
+
+func _on_PlayerButton_pressed():
+	$ProfileTab.visible = true
+	$MapTab.visible = false
+	$CharactersTab.visible = false
+	pass # Replace with function body.
+
+
+func _on_MapButton_pressed():
+	$ProfileTab.visible = false
+	$MapTab.visible = true
+	$CharactersTab.visible = false
+	pass # Replace with function body.
+
+
+func _on_CharactersButton_pressed():
+	$ProfileTab.visible = false
+	$MapTab.visible = false
+	$CharactersTab.visible = true
+	pass # Replace with function body.
+
+
+func _on_Character1Button_pressed():
+	$CharactersTab/CharacterZoom.texture = $CharactersTab/Character1.texture
+	$CharactersTab/CharacterZoom.scale = Vector2(0.316,0.316)
+	$CharactersTab/DescriptionText.text = ("Dona Suzana")
+	pass # Replace with function body.
+
+
+func _on_City1Button_pressed():
+	$MapTab/CityDescription.text = ("Núcleo Bandeirante")
+	pass # Replace with function body.
+
+
+func _on_City2Button_pressed():
+	$MapTab/CityDescription.text = ("Taguatinga")
+	pass # Replace with function body.
+
+
+func _on_City3Button_pressed():
+	$MapTab/CityDescription.text = ("Plano Piloto")
+	pass # Replace with function body.
