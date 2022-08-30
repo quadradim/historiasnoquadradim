66bed0dc0af83e85e409f6ddb6de3aa4ea50b4ef
uventura <ualivent@gmail.com>
2022-08-30 18:49:44 -0300

    Adding Multidão scene

---
+++ prototype/scenes/ambience/Multidao.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+var distractors_pressed = [0, 0, 0]
+
+func _ready():
+	pass
+
+func distractor1_pressed():
+	print("distractor 1")
+
+func distractor2_pressed():
+	print("distractor 2")
+
+func distractor3_pressed():
+	print("distractor 3")
