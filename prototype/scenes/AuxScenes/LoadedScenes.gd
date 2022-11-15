8032ac86025a3fff523f1305b7e498e0c8a386f8
uventura <ualivent@gmail.com>
2022-11-14 21:50:48 -0300

    Adding Global Audio

--- prototype/scenes/AuxScenes/LoadedScenes.gd
+++ prototype/scenes/AuxScenes/LoadedScenes.gd
@@ -3,110 +3,132 @@ var scenes_data = {
 #		'local',
 #		'emited_signal',
 #		'has_config'
+#		'audio' => null, 'path', 'continue'
 #	]
 	'production': [
 		'res://scenes/GUI/Production.tscn',
 		'end_production',
-		false
+		false,
+		null
 	],
 	'logo_opening': [
 		'res://scenes/GUI/LogoOpening.tscn',
 		'end_logo_scene',
-		false
+		false,
+		null
 	],
 	'menu': [
 		'res://scenes/GUI/Menu.tscn',
 		'end_menu',
-		false
+		false,
+		'res://soundtrack/Loop1 Intro.mp3'
 	],
 	'access01': [
 		'res://scenes/FirstAccess/Access01.tscn',
 		'end_access01',
 		true,
+		'continue'
 	],
 	'access02': [
 		'res://scenes/FirstAccess/Access02.tscn',
 		'end_access02',
-		true
+		true,
+		null
 	],
 	'character_choice': [
 		'res://scenes/FirstAccess/CharacterChoice.tscn',
 		'end_choice',
-		true
+		true,
+		null
 	],
 	'diary': [
 		'res://scenes/GUI/Diary.tscn',
 		'end_diary',
-		true
+		true,
+		null
 	],
 	'suzana': [
 		'res://scenes/dialogs/Suazana.tscn',
 		'end_suzana_scene',
-		true
+		true,
+		null
 	],
 	'episodes_selection': [
 		'res://scenes/GUI/EpisodeSelection.tscn',
 		'end_episode_selection',
-		false
+		false,
+		null
 	],
 	'analyze_photos': [
 		'res://scenes/observation/PhotoAnalyze.tscn',
 		'end_analyze',
-		false
+		false,
+		null
 	],
 	'lavadeiras': [
 		'res://scenes/ambience/Lavadeiras.tscn',
 		'end_lavadeira',
-		true
+		true,
+		null
 	],
 	'lavadeira_amiga': [
 		'res://scenes/dialogs/Lavadeira.tscn',
 		'end_lavadeira',
-		true
+		true,
+		null
 	],
 	'travel_scene': [
 		'res://scenes/GUI/TravelScene.tscn',
 		'end_travel_scene',
-		false
+		false,
+		null
 	],
 	'introd_lavadeiras': [
 		'res://scenes/Introductions/Lavadeiras.tscn',
 		'introd_lava',
-		false
+		false,
+		null
 	],
 	'intro_multidao': [
 		'res://scenes/Introductions/Multidao.tscn',
 		'end_multidao',
-		true
+		true,
+		null
 	],
 	'multidao': [
 		'res://scenes/ambience/Multidao.tscn',
 		'distractor_selected',
-		true
+		true,
+		null
 	],
 	'distractor1_darcy': [
 		'res://scenes/dialogs/protesto_bandeirante/Distrator01.tscn',
 		'distractor_dialog',
-		true
+		true,
+		null
 	],
 	'distractor2_darcy': [
 		'res://scenes/dialogs/protesto_bandeirante/Distrator02.tscn',
 		'distractor_dialog',
-		true
+		true,
+		null
 	],
 	'distractor3_darcy': [
 		'res://scenes/dialogs/protesto_bandeirante/Distrator03.tscn',
 		'distractor_dialog',
-		true
+		true,
+		null
 	],
 	'darcy_speech': [
 		'res://scenes/dialogs/Darcy.tscn',
 		'end_darcy_speech',
-		true
+		true,
+		null
 	],
 	'word_choice': [
 		'res://scenes/Minigames/WordSelection.tscn',
 		'end_word_selection',
-		true
+		true,
+		null
 	],
 }
