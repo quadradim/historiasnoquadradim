1573938ec5b59f26fec0bcf986fcb3259d14018b
lucaSena <87825212+lucaSena@users.noreply.github.com>
2022-11-28 18:51:30 -0300

    PhotoAnalyze implementation

--- prototype/scenes/AuxScenes/ChatBox.gd
+++ prototype/scenes/AuxScenes/ChatBox.gd
@@ -1,13 +1,20 @@
 extends Control
 
 export(String, FILE) var chat_res
+export(String, MULTILINE) var chat
 signal end_dialog
 signal new_dialog
 signal Display
+signal change_face1
+signal change_face2
+signal change_face3
+signal change_face4
 
 export (Array, Resource) var img
 export (Array, String) var cha
 export (Array, Vector2) var image_scales
+export (Array, String) var nam
+export (Array, Vector2) var resize_panel
 
 var profile_image_indices = []
 var text_position = 0
@@ -19,13 +26,33 @@ var line_counter = 0
 
 export(bool) var has_profile
 
-func load_file(file_src):
-	var file = File.new()
-	file.open(file_src, File.READ)
+#func load_file(file_src):
+#	var file = File.new()
+#	file.open(file_src, File.READ)
+#	var dialog = ""
+#	while not file.eof_reached():
+#		var is_character_line = false
+#		var current_line = file.get_line()
+#		for i  in  range (len(cha)):
+#			if cha[i] == current_line:
+#				line_counter += 1
+#				profile_image_indices.append(i)
+#				is_character_line = true
+#				if dialog != "":
+#					current_dialog.append(dialog)
+#					dialog = ""
+#		if is_character_line == false:
+#			dialog += current_line
+#	if len(dialog) > 0:
+#		current_dialog.append(dialog)
+#	file.close()
+	
+func load_chat(entire_chat):
+	entire_chat = entire_chat.split('*')
 	var dialog = ""
-	while not file.eof_reached():
+	while line_counter < len(entire_chat):
 		var is_character_line = false
-		var current_line = file.get_line()
+		var current_line = entire_chat[line_counter]
 		for i  in  range (len(cha)):
 			if cha[i] == current_line:
 				line_counter += 1
@@ -36,9 +63,10 @@ func load_file(file_src):
 					dialog = ""
 		if is_character_line == false:
 			dialog += current_line
+			line_counter += 1
 	if len(dialog) > 0:
 		current_dialog.append(dialog)
-	file.close()
+	$Writting_Timer.start()
 
 func reset_current_text():
 	$ChatText.text = ''
@@ -48,21 +76,29 @@ func reset_current_text():
 func _ready():
 #	image_scale[0] = Vector2(0.05,0.05)
 #	image_scale[1] = Vector2(0.175,0.175)
-	
-	load_file(str(chat_res))
+#	load_file(str(chat_res))
+	load_chat(chat)
 	if has_profile:
-		$ProfileImages.display(
-			img[profile_image_indices[text_position]],
-			image_scales[profile_image_indices[text_position]]
-#			Vector2(0.05,0.05)
-		)
-
+		if profile_image_indices[text_position] == 0:
+			$MainCharacterChatBox.visible = true
+			$SupportingCharacterChatBox.visible = false
+		else:
+			$MainCharacterChatBox.visible = false
+			$SupportingCharacterChatBox.visible = true
+			$SupportingCharacterChatBox/ProfileImages/Panel/Label.text = nam[profile_image_indices[text_position]]
+			$SupportingCharacterChatBox/ProfileImages.display(
+				img[profile_image_indices[text_position]],
+				image_scales[profile_image_indices[text_position]],
+				resize_panel[profile_image_indices[text_position]]
+			)
+	
 func hide_profile():
 	has_profile = false
-	$ProfileImages.hide()
+	$SupportingCharacterChatBox/ProfileImages.hide()
 
 func start():
 	$Timer.start()
+	
 
 func Time_to_write():
 	if text_position < len(current_dialog) \
@@ -73,17 +109,32 @@ func Time_to_write():
 	$Timer.start()
 
 func next_dialog():
-	if text_position < line_counter-1:
+	if text_position < line_counter/2 -1:
 		reset_current_text()
 		text_position += 1
-		
+		if text_position == 1:
+			emit_signal("change_face2")
+		if text_position == 3:
+			emit_signal("change_face1")
+		if text_position == 5:
+			emit_signal("change_face3")
+		if text_position == 7:
+			emit_signal("change_face2")
+		if text_position == 9:
+			emit_signal("change_face4")
 		if has_profile:
-			$ProfileImages.display(
-				img[profile_image_indices[text_position]],
-				image_scales[profile_image_indices[text_position]]
-#				image_scale[profile_image_indices[text_position]]
-			)
-		
+			if profile_image_indices[text_position] == 0:
+				$MainCharacterChatBox.visible = true
+				$SupportingCharacterChatBox.visible = false
+			else:
+				$MainCharacterChatBox.visible = false
+				$SupportingCharacterChatBox.visible = true
+				$SupportingCharacterChatBox/ProfileImages/Panel/Label.text = nam[profile_image_indices[text_position]]
+				$SupportingCharacterChatBox/ProfileImages.display(
+					img[profile_image_indices[text_position]],
+					image_scales[profile_image_indices[text_position]],
+					resize_panel[profile_image_indices[text_position]]
+				)
 		emit_signal("new_dialog")
 	else:
 		emit_signal("end_dialog")
@@ -94,8 +145,20 @@ func previous_dialog():
 		text_position -= 1
 		
 		if has_profile:
-			$ProfileImages.display(
-				img[profile_image_indices[text_position]],
-				image_scales[profile_image_indices[text_position]]
-#				image_scale[profile_image_indices[text_position]]
-			)
+			if profile_image_indices[text_position] == 0:
+				$MainCharacterChatBox.visible = true
+				$SupportingCharacterChatBox.visible = false
+			else:
+				$MainCharacterChatBox.visible = false
+				$SupportingCharacterChatBox.visible = true
+				$SupportingCharacterChatBox/ProfileImages/Panel/Label.text = nam[profile_image_indices[text_position]]
+				$SupportingCharacterChatBox/ProfileImages.display(
+					img[profile_image_indices[text_position]],
+					image_scales[profile_image_indices[text_position]],
+					resize_panel[profile_image_indices[text_position]]
+				)
+
+
+func _on_Writting_Timer_timeout():
+	Time_to_write()
+	pass # Replace with function body.
--- prototype/scenes/AuxScenes/ProfileImages.gd
+++ prototype/scenes/AuxScenes/ProfileImages.gd
@@ -3,9 +3,10 @@ extends Node2D
 func _ready():
 	pass
 	
-func display(image, scale_def):
+func display(image, scale_def, size_panel):
 	$Image.scale = scale_def
 	$Image.texture = image
+	$Panel.rect_size = size_panel
 
 func _on_ChatBox_suzana_dialog():
 	pass
