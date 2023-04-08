5b18719866e959add18a5e4ea7b5d0bd10bb807a
uventura <ualivent@gmail.com>
2023-04-07 22:03:16 -0300

    Removing unnecessary comments

--- prototype/scenes/dialogs/Darcy.gd
+++ prototype/scenes/dialogs/Darcy.gd
@@ -4,26 +4,14 @@ var player_data
 
 func _ready():
 	player_data = $PlayerBackPack/DiaryInventory/PlayerEntitiy.read()
-	pass # Replace with function body.
 
 func start():
 	$ChatBox.start()
 	
 func end_speech():
 	$PlayerBackPack/DiaryInventory/PlayerEntitiy.modifier("characters",2)
-#	$PlayerBackPack/DiaryInventory/PlayerEntitiy.insert(
-#	{
-#		"name": player_data["name"],
-#		"habilities": player_data["habilities"],
-#		"backpack": player_data["backpack"],
-#		"ability": [1,0,0,0,0,0,0,0,0],
-#		"historiometer":3,
-#		"characters":1,
-#		"soundtrack": player_data["soundtrack"],
-#		"soundeffect": player_data["soundeffect"]
-#	}
-#)
 	$PlayerBackPack/DiaryInventory._ready()
+
 	emit_signal("end_darcy_speech", "word_choice")
 
 func _process(delta):
